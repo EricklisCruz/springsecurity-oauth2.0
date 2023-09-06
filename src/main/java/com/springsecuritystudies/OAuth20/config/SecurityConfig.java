1d6f966d78fd76e0906655a68eff76cbd23d455a
EricklisCruz <erickliscruz@gmail.com>
2023-09-06 14:54:14 -0300

    created class SecurityConfig.java

---
+++ src/main/java/com/springsecuritystudies/OAuth20/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springsecuritystudies.OAuth20.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
+}
